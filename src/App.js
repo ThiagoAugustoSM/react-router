ece8f12ce9321fca60ba4925875a885294051172
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2020-02-17 10:30:17 -0500

    Aula 7

--- src/App.js
+++ src/App.js
@@ -7,18 +7,21 @@ import ErrorScreen from './views/ErrorScreen';
 import ProductScreen from './views/ProductScreen';
 
 import {BrowserRouter as Router,
-        Route
+        Route,
+        Switch
         } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route exact path='/' component={HomeScreen}/>
-        <Route path='/carrinhodecompras' component={ShoppingCartScreen}/>
-        <Route path='/categoria' component={CategoryScreen}/>
-        <Route path='/error' component={ErrorScreen}/>
-        <Route path='/produto' component={ProductScreen}/>
+        <Switch>
+          <Route exact path='/' component={HomeScreen}/>
+          <Route path='/carrinhodecompras' component={ShoppingCartScreen}/>
+          <Route path='/categoria' component={CategoryScreen}/>
+          <Route path='/error' component={ErrorScreen}/>
+          <Route path='/produto' component={ProductScreen}/>
+        </Switch>
       </Router>
     </div>
   );
