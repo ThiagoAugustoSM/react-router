9153617c9958f321369fe30db8b6c5c0776e7c3a
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2020-02-17 09:51:27 -0500

    aula 5

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import HomeScreen from './views/HomeScreen';
 import ShoppingCartScreen from './views/ShoppingCartScreen';
 import CategoryScreen from './views/CategoryScreen';
 import ErrorScreen from './views/ShoppingCartScreen';
+import ProductScreen from './views/ProductScreen';
 
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <HomeScreen/>
-      <ShoppingCartScreen/>
-      <CategoryScreen/>
-      <ErrorScreen/>
+      <Router>
+        <HomeScreen/>
+        <ShoppingCartScreen/>
+        <CategoryScreen/>
+        <ErrorScreen/>
+        <ProductScreen/>
+      </Router>
     </div>
   );
 }
---
+++ src/views/ProductScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProductScreen = (props) => {
+  return (
+    <h1>ProductScreen</h1>
+  )
+}
+
+export default ProductScreen;
