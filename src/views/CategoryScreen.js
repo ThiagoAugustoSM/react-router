afe89c1adbcd4075adefbab49b9467252dfab086
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2020-02-17 12:34:22 -0500

    aula 15

--- src/views/CategoryScreen.js
+++ src/views/CategoryScreen.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
+import { useLocation } from 'react-router-dom';
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
 const CategoryScreen = (props) => {
+
+  let query = useQuery();
+
   return (
+    <>
+    
     <h1>CategoryScreen</h1>
+    {query.get('name') && <h1>Name: {query.get('name')}</h1>}
+    {query.get('promocao') && <h1>Promoção: {query.get('promocao')}</h1>}
+    </>
   )
 }
 
