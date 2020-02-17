c51cd6b4cdc140862755f326654a213758bd1cdc
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2020-02-17 11:38:52 -0500

    aula 12

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Link, NavLink, useHistory } from 'react-router-dom';
+import { useHistory, Prompt } from 'react-router-dom';
 
 import './Header.css';
 
@@ -9,15 +9,33 @@ import * as ROUTES from '../constants/Routes';
 const Header = (props) => {
 
   let history = useHistory();
+  let [isBlocking, setIsBlocking] = useState(true);
 
   const goToHome = () => {
     history.push(ROUTES.HOME_SCREEN);
   }
 
+  const handleSubmit = () => {
+    alert('Foi Submetido')
+  }
+
   return (
-    <div onClick={goToHome} className='logo'>
-      <h1>Platzi Store</h1>
-    </div>
+    <>
+      <div onClick={goToHome} className='logo'>
+        <h1>Platzi Store</h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder='O que você quer pesquisar?'
+        >
+        </input>
+        <Prompt
+          when={isBlocking}
+          message={() => 'Você tem certeza que deseja sair dessa tela? A pesquisa não foi concluída.'}
+        />
+
+      </form>
+    </>
   )
 }
 
