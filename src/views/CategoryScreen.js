9d8e223ee957fbe127b1b0dde37163eb8f841859
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2020-02-18 10:41:51 -0500

    aula 24

--- src/views/CategoryScreen.js
+++ src/views/CategoryScreen.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 import { useLocation } from 'react-router-dom';
 
+import ProductCard from '../components/ProductCard';
+
+import './CategoryScreen.css';
+
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
@@ -12,10 +16,17 @@ const CategoryScreen = (props) => {
 
   return (
     <>
-    
-    <h1>CategoryScreen</h1>
-    {query.get('name') && <h1>Name: {query.get('name')}</h1>}
-    {query.get('promocao') && <h1>Promoção: {query.get('promocao')}</h1>}
+    <div className='container__bodyCategory'>
+      {query.get('promocao') && <h1>VENHA APROVEITAR AS PROMOÇÕES!!</h1>}
+      {query.get('name') && <h1>Categoria: {query.get('name')}</h1>}
+      <div className='container__categoryScreen'>
+      <ProductCard name='Relogio' price='12,00' imgUrl='https://images.asos-media.com/products/bellfield-mens-chronograph-bracelet-watch-in-gold/13215289-1-gold'/>
+      <ProductCard name='Relogio' price='12,00' imgUrl='https://images.asos-media.com/products/bellfield-mens-chronograph-bracelet-watch-in-gold/13215289-1-gold'/>
+      <ProductCard name='Relogio' price='12,00' imgUrl='https://images.asos-media.com/products/bellfield-mens-chronograph-bracelet-watch-in-gold/13215289-1-gold'/>
+      <ProductCard name='Relogio' price='12,00' imgUrl='https://images.asos-media.com/products/bellfield-mens-chronograph-bracelet-watch-in-gold/13215289-1-gold'/>
+      <ProductCard name='Relogio' price='12,00' imgUrl='https://images.asos-media.com/products/bellfield-mens-chronograph-bracelet-watch-in-gold/13215289-1-gold'/>
+      </div>
+    </div>    
     </>
   )
 }
