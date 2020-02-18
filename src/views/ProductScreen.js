327becc92459498047856e12769d84695e3d0b8c
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2020-02-18 10:24:58 -0500

    aula 22

--- src/views/ProductScreen.js
+++ src/views/ProductScreen.js
@@ -2,14 +2,32 @@ import React from 'react';
 
 import { useParams } from 'react-router-dom';
 
+import './ProductScreen.css';
+
 const ProductScreen = (props) => {
 
   let { id } = useParams();
 
   return (
     <>
-    <h1>ProductScreen:</h1>
-    <h1>ID: {id}</h1>
+    <div className='container__product'>
+      <div className='imageContainer'>
+        <img src='https://images.asos-media.com/products/bellfield-mens-chronograph-bracelet-watch-in-gold/13215289-1-gold'></img>
+      </div>
+      <div className='textContainer'>
+        <h1>Relógio de Ouro</h1>
+        <p>Produto ID: {id}</p>
+
+        <div className='priceContainer'>
+          <p>R$ 12,00</p>
+          <button>Adicionar ao Carrinho</button>
+        </div>
+
+        <div className='productContainer'>
+          <p>Descrição do Item</p>
+        </div>
+      </div>
+    </div>
     </>
    )
 }
