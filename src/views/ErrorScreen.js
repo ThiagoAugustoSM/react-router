e2d6e9ff1d10eb611286f33ec335ddba0f3dc514
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2020-02-19 10:23:22 -0500

    aula 33

--- src/views/ErrorScreen.js
+++ src/views/ErrorScreen.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
+import { useHistory } from 'react-router-dom';
+
+import './ErrorScreen.css';
+
 const ErrorScreen = (props) => {
+
+  let history = useHistory();
+
+  const goBack = () => history.goBack();
+
   return (
-    <h1>ErrorScreen</h1>
+    <>
+    <div className='container__error'>
+      <img src='./error404.png'></img>
+      <div className='text'>
+        <h2>A gente não achou essa página que você estava procurando. Gostaria de continuar na plataforma?</h2>
+        <input onClick={goBack} type='button' value='Voltar para a página anterior'></input>
+      </div>
+    </div>
+    </>
   )
 }
 
